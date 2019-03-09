dbd03e16c1d459a6f18bf6f48112c0e9d46e7da3
Heiman Chan <heimanchan0@gmail.com>
2019-03-09 14:57:29 -0800

    Setup users route

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-
 const express = require("express");
 const app = express();
 const db = require('./config/key').mongoURI;
+const bodyParser = require('body-parser');
+
+// Import routes
+const users = require("./routes/api/users");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -11,6 +14,12 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello PiCook"));
 
-const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// routes used
+app.use("/api/users", users);
+
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
