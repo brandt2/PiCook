757ee9d31273543083906bf047f64f043ca8d55d
Heiman Chan <heimanchan0@gmail.com>
2019-03-09 14:37:30 -0800

    Setup mongoose

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+const mongoose = require('mongoose');
+
 const express = require("express");
 const app = express();
+const db = require('./config/key').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello PiCook"));
 
---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb+srv://heimanchan:<password>@cluster0-rwiti.mongodb.net/test?retryWrites=true"
+}
