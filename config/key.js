38efa418f1f5643da1d6b27ef8ee9b16e32c0f39
Heiman Chan <heimanchan0@gmail.com>
2019-03-09 16:01:54 -0800

    Remove node_modules

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://heimanchan:<password>@cluster0-rwiti.mongodb.net/test?retryWrites=true"
+  mongoURI: "mongodb+srv://heimanchan:Heiman001@cluster0-rwiti.mongodb.net/test?retryWrites=true"
 }
---
+++ models/Recipe.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema ({
+
+})
+
+module.exports = Recipe = mongoose.model('recipes', RecipeSchema);
