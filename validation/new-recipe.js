64d3b742fe71f94c1138abbe2bbab84bc9ee5994
Young Chul Lim <youngchullim95@gmail.com>
2019-03-10 01:19:43 -0800

    Update Validations for Recipe

--- validation/new-recipe.js
+++ validation/new-recipe.js
@@ -5,9 +5,20 @@ modele.exports = function validateNewRecipeInput(data) {
   let errors = {};
 
   data.title = validText(data.title) ? data.title : "";
+  data.instructions = validText(data.instructions) ? data.instructions : "";
+  data.ingredients = validText(data.ingredients) ? data.ingredients : "";
+
   
   if (Validator.isEmpty(data.title)) {
     errors.title = "Title field is empty";
+  }
+  
+  if (Validator.isEmpty(data.instructions)) {
+    errors.instructions = "Instructions field is empty";
+  }
+  
+  if (Validator.isEmpty(data.ingredients)) {
+    errors.ingredients = "Ingredients field is empty";
   }
 
   return {
