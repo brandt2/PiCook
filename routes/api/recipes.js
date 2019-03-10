d5b014ee03297881990c640e9fcfdae1ac8bd730
Young Chul Lim <youngchullim95@gmail.com>
2019-03-10 03:04:08 -0700

    Update Index and Show routes to check if logged in

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -9,23 +9,32 @@ const Recipe = require('../../models/Recipe');
 const validateNewRecipeInput =  require('../../validation/new-recipe');
 
 // INDEX all recipes
-router.get("/", (req, res) => {
-  Recipe.find()
-    .sort({date: -1})
-    .then(recipe => res.json(recipe))
-    .catch(err => res.status(404).json({norecipesfound: "No recipes found"}));
-});
+// route '/recipe' ???
+router.get("/", 
+  passport.authenticate('jwt', {session: false}),
+  (req, res) => {
+    Recipe.find()
+      .sort({date: -1})
+      .then(recipe => res.json(recipe))
+      .catch(err => res.status(404).json({norecipesfound: "No recipes found"}));
+  }
+);
 
 // SHOW one recipe
-router.get('/:id', (req, res) => {
-  Recipe.findById(req.params.id)
-    .then(recipe => res.json(recipe))
-    .catch(err => 
-      res.status(404).json({norecipefound: "No recipe found with that ID"}));
-});
+// route '/recipe/:id' ???
+router.get('/:id', 
+  passport.authenticate('jwt', {session: false}),
+  (req, res) => {
+    Recipe.findById(req.params.id)
+      .then(recipe => res.json(recipe))
+      .catch(err => 
+        res.status(404).json({norecipefound: "No recipe found with that ID"}));
+  }
+);
 
 
 // CREATE new recipe
+// route '/recipe/new' ???
 router.post("/", 
   passport.authenticate('jwt', {session: false}),
   (req, res) => {
