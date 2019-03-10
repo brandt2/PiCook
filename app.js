1f0a24867cc9ed6d9b28d064d84f1b5052e00d34
Heiman Chan <heimanchan0@gmail.com>
2019-03-09 16:40:30 -0800

    Add recipe model

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
-const db = require('./config/key').mongoURI;
+const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 
 // Import routes
