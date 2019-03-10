bdf8ba588c14f7e60fe71d52e118110216bd3b5e
Brandon Tran <brandt2@uci.edu>
2019-03-10 01:34:11 -0800

    Add register route to users routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ email: "A user has already registered with this email address"})
+      } else {
+        const newUser = new User({
+          username: req.user.username,
+          email: req.body.email,
+          password: req.body.password,
+          fname: req.body.fname,
+          lname: req.body.lname
+        })
+
+        newUser.save().then(user => res.send(user)).catch(err => res.send(err));
+      }
+    })
+})
 
 module.exports = router;
