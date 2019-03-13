cfe432aea6c7421db632a6cfeb1bd78530a565a4
Brandon Tran <brandt2@uci.edu>
2019-03-13 11:18:55 -0700

    Add loading animation to categories

--- frontend/src/components/meals_db_recipes/db_categories.js
+++ frontend/src/components/meals_db_recipes/db_categories.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LoadingIcon from './loading_icon.js';
 
 class DBCategories extends React.Component{
   constructor(props) {
     super(props);
+
+    this.state = {
+      loading: true
+    }
   }
   componentDidMount() {
     this.props.getAllCategories()
   }
 
+  componentDidUpdate({ loading }) {
+    if (this.props.loading !== this.state.loading) {
+      this.setState({ loading: this.props.loading })
+    }
+  }
+
   render() {
     const recipes = this.props.recipes;
     if (recipes === undefined) return null;
+
+    if (this.state.loading === true) {
+      return (
+        <LoadingIcon />
+      )
+    }
     
     const categories = recipes.map(cat => {
       if (cat.strCategory === "Pork") cat.strCategoryThumb = "https://www.themealdb.com/images/category/pork.png"
